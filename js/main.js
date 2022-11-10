6e298aab5259b649f7f1155d001ea50885cfbf86
zumarRamadhan <zumar.ramadhan46@gmail.com>
2022-11-10 20:57:59 +0700

    style[ZR]: create menu page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,45 @@
-let cursor = document.querySelector(".cursor");
-let cursorInner = document.querySelector(".cursorInner");
-document.addEventListener("mousemove", (event) => {
-  cursor.style.cssText = cursorInner.style.cssText =
-    "left: " + event.clientX + "px; top: " + event.clientY + "px;";
+// nav active click
+var header = document.getElementById("tabbar");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
+
+
+const showOnPx = 100;
+const backToTopButton = document.querySelector(".back-to-top");
+const pageProgressBar = document.querySelector(".progress-bar");
+
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
+
+const goToTop = () => {
+  document.body.scrollIntoView({
+    behavior: "smooth"
+  });
+};
+
+document.addEventListener("scroll", () => {
+  console.log("Scroll Height: ", scrollContainer().scrollHeight);
+  console.log("Client Height: ", scrollContainer().clientHeight);
+
+  const scrolledPercentage =
+    (scrollContainer().scrollTop /
+      (scrollContainer().scrollHeight - scrollContainer().clientHeight)) *
+    100;
+
+  pageProgressBar.style.width = `${scrolledPercentage}%`;
+
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden");
+  } else {
+    backToTopButton.classList.add("hidden");
+  }
 });
+
+backToTopButton.addEventListener("click", goToTop);
