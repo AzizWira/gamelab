5f57692b45fa3b869f5e4ecf46e14e5a85f9c43f
AZ <wira123widodo@gmail.com>
2022-11-13 13:43:24 +0700

    style[AW]: create responsive testimonial

--- js/main.js
+++ js/main.js
@@ -1,49 +1,49 @@
 // function cursor
 
-let cursor = document.querySelector(".cursor");
-let cursorInner = document.querySelector(".cursorInner");
-document.addEventListener("mousemove", (event) => {
+let cursor = document.querySelector('.cursor')
+let cursorInner = document.querySelector('.cursorInner')
+document.addEventListener('mousemove', (event) => {
   cursor.style.cssText = cursorInner.style.cssText =
-    "left: " + event.clientX + "px; top: " + event.clientY + "px;";
-});
+    'left: ' + event.clientX + 'px; top: ' + event.clientY + 'px;'
+})
 
 // end of function cursor
 
 // funchion progress bar and to top
 
-const showOnPx = 100;
-const backToTopButton = document.querySelector(".back-to-top");
-const pageProgressBar = document.querySelector(".progress-bar");
+const showOnPx = 100
+const backToTopButton = document.querySelector('.back-to-top')
+const pageProgressBar = document.querySelector('.progress-bar')
 
 const scrollContainer = () => {
-  return document.documentElement || document.body;
-};
+  return document.documentElement || document.body
+}
 
 const goToTop = () => {
   document.body.scrollIntoView({
-    behavior: "smooth"
-  });
-};
+    behavior: 'smooth',
+  })
+}
 
-document.addEventListener("scroll", () => {
-  console.log("Scroll Height: ", scrollContainer().scrollHeight);
-  console.log("Client Height: ", scrollContainer().clientHeight);
+document.addEventListener('scroll', () => {
+  console.log('Scroll Height: ', scrollContainer().scrollHeight)
+  console.log('Client Height: ', scrollContainer().clientHeight)
 
   const scrolledPercentage =
     (scrollContainer().scrollTop /
       (scrollContainer().scrollHeight - scrollContainer().clientHeight)) *
-    100;
+    100
 
-  pageProgressBar.style.width = `${scrolledPercentage}%`;
+  pageProgressBar.style.width = `${scrolledPercentage}%`
 
   if (scrollContainer().scrollTop > showOnPx) {
-    backToTopButton.classList.remove("hidden-top");
+    backToTopButton.classList.remove('hidden-top')
   } else {
-    backToTopButton.classList.add("hidden-top");
+    backToTopButton.classList.add('hidden-top')
   }
-});
+})
 
-backToTopButton.addEventListener("click", goToTop);
+backToTopButton.addEventListener('click', goToTop)
 
 // end of function progress bar and to top
 
@@ -103,36 +103,48 @@ $(document).ready(function () {
 
 // funchion menu
 
-
-
 // end of function menu
 
 // function tabbar
-var header = document.getElementById("tabbar-menu");
-var btns = header.getElementsByClassName("btn-menu");
+var header = document.getElementById('tabbar-menu')
+var btns = header.getElementsByClassName('btn-menu')
 for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active-tabbar");
-  current[0].className = current[0].className.replace(" active-tabbar", "");
-  this.className += " active-tabbar";
-  });
+  btns[i].addEventListener('click', function () {
+    var current = document.getElementsByClassName('active-tabbar')
+    current[0].className = current[0].className.replace(' active-tabbar', '')
+    this.className += ' active-tabbar'
+  })
 }
 // end of function tabbar
 
 // function testimonial slider
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper('.mySwiper', {
   slidesPerView: 3,
   spaceBetween: 30,
   slidesPerGroup: 3,
   loop: true,
   loopFillGroupWithBlank: true,
   pagination: {
-    el: ".swiper-pagination",
+    el: '.swiper-pagination',
     clickable: true,
   },
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
-});
+  breakpoints: {
+    2000: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+    },
+    900: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+    },
+    600: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+  },
+})
 // end funchion slider
