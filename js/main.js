77fe3ef746b26206d17bd21ac871a931645b8ef3
AZ <wira123widodo@gmail.com>
2022-11-18 00:28:17 +0700

    style[AW]: create menu

--- js/main.js
+++ js/main.js
@@ -152,3 +152,28 @@ var swiper = new Swiper('.mySwiper', {
   },
 })
 // end funchion slider
+
+// funchion menu
+let preveiwContainer = document.querySelector('.products-preview');
+let previewBox = preveiwContainer.querySelectorAll('.preview-card');
+
+document.querySelectorAll('.products-container .card-product').forEach(product =>{
+  product.onclick = () =>{
+    preveiwContainer.style.display = 'flex';
+    let name = product.getAttribute('data-name');
+    previewBox.forEach(preview =>{
+      let target = preview.getAttribute('data-target');
+      if(name == target){
+        preview.classList.add('active-preview');
+      }
+    });
+  };
+});
+
+previewBox.forEach(close =>{
+  close.querySelector('.fa-times').onclick = () =>{
+    close.classList.remove('active-preview');
+    preveiwContainer.style.display = 'none';
+  };
+});
+// end funchion menu
